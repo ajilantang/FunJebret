de1ea3083178372cf64938bb5e513558638a0277
aditya wira nugraha <aditya_wira98@yahoo.com>
2017-08-22 16:15:36 +0700

    add signout

--- app/src/main/java/com/task/basilischi/funjebret/LoginActivity.java
+++ app/src/main/java/com/task/basilischi/funjebret/LoginActivity.java
@@ -1,13 +1,19 @@
 package com.task.basilischi.funjebret;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.facebook.login.LoginManager;
 
 public class LoginActivity extends AppCompatActivity {
 
     TextView textView;
+    Button signout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +22,14 @@ public class LoginActivity extends AppCompatActivity {
         String status = intent.getStringExtra("loginStatus");
         textView = (TextView)findViewById(R.id.textView);
         textView.setText(status);
+        signout = (Button)findViewById(R.id.signOut);
+        signout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent back = new Intent(LoginActivity.this, MainActivity.class);
+                LoginManager.getInstance().logOut();
+                finish();
+            }
+        });
     }
 }
