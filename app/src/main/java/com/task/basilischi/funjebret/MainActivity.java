0f2c97cc1fcb8bbf4bf91cdbbce60d9f72e0ecc3
aditya wira nugraha <aditya_wira98@yahoo.com>
2017-08-22 11:27:09 +0700

    add login facebook

--- app/src/main/java/com/task/basilischi/funjebret/MainActivity.java
+++ app/src/main/java/com/task/basilischi/funjebret/MainActivity.java
@@ -1,13 +1,53 @@
 package com.task.basilischi.funjebret;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    LoginButton loginButton;
+    TextView textView;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+        textView = (TextView)findViewById(R.id.textView);
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // change to main app and delete this  
+                textView.setText("Your id \n"+ loginResult.getAccessToken().getUserId() +
+                "\n"+"Your Token access \n"+loginResult.getAccessToken().getToken());
+            }
+
+            @Override
+            public void onCancel() {
+                textView.setText("Login Failed!!");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        callbackManager.onActivityResult(requestCode,resultCode,data);
     }
 }
