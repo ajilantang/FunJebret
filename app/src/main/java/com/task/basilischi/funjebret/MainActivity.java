c6731e03b2f1acd9d75caf1abda03f1b31360d50
aditya wira nugraha <aditya_wira98@yahoo.com>
2017-08-29 15:11:25 +0700

    refactor main to main login to login

--- app/src/main/java/com/task/basilischi/funjebret/MainActivity.java
+++ app/src/main/java/com/task/basilischi/funjebret/MainActivity.java
@@ -2,173 +2,85 @@ package com.task.basilischi.funjebret;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.facebook.AccessToken;
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.analytics.FirebaseAnalytics;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FacebookAuthProvider;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import it.neokree.materialtabs.MaterialTab;
+import it.neokree.materialtabs.MaterialTabHost;
+import it.neokree.materialtabs.MaterialTabListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MaterialTabListener {
 
-    @BindView(R.id.signIn) Button signin;
-    @BindView(R.id.login_button) LoginButton loginButton;
-    @BindView(R.id.signUp) TextView signup;
-    @BindView(R.id.inputEmail) EditText email;
-    @BindView(R.id.inputPassword) EditText pass;
+    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.viewpager) ViewPager viewPager;
+    @BindView(R.id.tabHost) MaterialTabHost tabHost;
 
-//    @BindView(R.id.titleApp) TextView textView;
-//    Typeface tf;
-
-    String emailStr, passStr;
-    DatabaseHelper helper;
-    CallbackManager callbackManager;
-    private FirebaseAuth firebaseAuth;
-    private FirebaseAuth.AuthStateListener firebaseAuthListener;
-    private FirebaseAnalytics mFirebaseAnalytics;
+    private PageMenuFragmentAdapter menuAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
 
-        helper = new DatabaseHelper(this);
-        loginButton = (LoginButton)findViewById(R.id.login_button);
-        signin = (Button)findViewById(R.id.signIn);
-        signup = (TextView) findViewById(R.id.signUp);
-        email = (EditText)findViewById(R.id.inputEmail);
-        pass = (EditText)findViewById(R.id.inputPassword);
-        firebaseAuth = FirebaseAuth.getInstance();
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        // set toolbar
+        setSupportActionBar(toolbar);
 
-        callbackManager = CallbackManager.Factory.create();
-        //tf = Typeface.createFromAsset(getAssets(),"RECOGNITION.ttf");
-        //textView.setTypeface(tf);
-
-        firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+        menuAdapter = new PageMenuFragmentAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(menuAdapter);
+        viewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
             @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    // User is signed in
-                    Log.d("signin", "onAuthStateChanged:signed_in:" + user.getUid());
-                } else {
-                    // User is signed out
-                    Log.d("signout", "onAuthStateChanged:signed_out");
-                }
-                // ...
-            }
-        };
-
-        signin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                emailStr = email.getText().toString();
-                passStr = pass.getText().toString();
-
-                String password = helper.searchPass(passStr);
-
-                if(passStr.equals(password)){
-                    ActivityCompat.finishAffinity(MainActivity.this);
-                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                    intent.putExtra("email",emailStr);
-                    startActivity(intent);
-                }else{
-                    Toast item = Toast.makeText(MainActivity.this, "Email or Password is Wrong!!", Toast.LENGTH_SHORT);
-                    item.show();
-                }
+            public void onPageSelected(int tabPosition) {
+                tabHost.setSelectedNavigationItem(tabPosition);
             }
         });
 
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SignUp.class);
-                startActivity(intent);
-            }
-        });
+        //Set color background tab
+        tabHost.setAccentColor(getResources().getColor(R.color.primary_dark));
 
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-//                intent.putExtra("loginStatus", "Your id \n"+ loginResult.getAccessToken().getUserId() +
-//                        "\n"+"Your Token access \n"+loginResult.getAccessToken().getToken());
-                handleFacebookAccessToken(loginResult.getAccessToken());
-            }
+        //for tab position
+        for (int i = 0; i < menuAdapter.getCount(); i++) {
+            tabHost.addTab(
+                    tabHost.newTab()
+                            .setText(menuAdapter.getPageTitle(i))
+                            .setTabListener(this)
+            );
 
-            @Override
-            public void onCancel() {
-                Toast.makeText(MainActivity.this, "Login Canceled!!", Toast.LENGTH_SHORT).show();
-            }
 
-            @Override
-            public void onError(FacebookException error) {
-                Toast.makeText(MainActivity.this, "No Network Internet!!", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    private void handleFacebookAccessToken(AccessToken token){
-        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
-        firebaseAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            Toast.makeText(MainActivity.this, "Auth Success!", Toast.LENGTH_SHORT).show();
-                            firebaseAuth.addAuthStateListener(firebaseAuthListener);
-                            ActivityCompat.finishAffinity(MainActivity.this);
-                            Intent intent = new Intent(MainActivity.this,LoginActivity.class);
-                            startActivity(intent);
-                        }else{
-                            Toast.makeText(MainActivity.this, "Auth Failed!", Toast.LENGTH_SHORT).show();
-                            firebaseAuth.removeAuthStateListener(firebaseAuthListener);
-                        }
-                    }
-                });
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        firebaseAuth.addAuthStateListener(firebaseAuthListener);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (firebaseAuthListener != null) {
-            firebaseAuth.removeAuthStateListener(firebaseAuthListener);
+            Intent intent = getIntent();
+            String status = intent.getStringExtra("loginStatus");
         }
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        callbackManager.onActivityResult(requestCode,resultCode,data);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
     }
+
+    //tab on selected
+        @Override
+        public void onTabSelected(MaterialTab materialTab) {
+
+            viewPager.setCurrentItem(materialTab.getPosition());
+        }
+
+        //tab on reselected
+        @Override
+        public void onTabReselected(MaterialTab materialTab) {
+
+        }
+
+        //tab on unselected
+        @Override
+        public void onTabUnselected(MaterialTab materialTab) {
+
+        }
 }
