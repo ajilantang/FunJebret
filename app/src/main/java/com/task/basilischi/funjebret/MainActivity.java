bf39cf1a876fba8e4e43c7e7cbbd6429f60037dc
aditya wira nugraha <aditya_wira98@yahoo.com>
2017-08-29 14:54:18 +0700

    connect to firebase

--- app/src/main/java/com/task/basilischi/funjebret/MainActivity.java
+++ app/src/main/java/com/task/basilischi/funjebret/MainActivity.java
@@ -2,20 +2,31 @@ package com.task.basilischi.funjebret;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -34,6 +45,9 @@ public class MainActivity extends AppCompatActivity {
     String emailStr, passStr;
     DatabaseHelper helper;
     CallbackManager callbackManager;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,10 +58,32 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         helper = new DatabaseHelper(this);
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+        signin = (Button)findViewById(R.id.signIn);
+        signup = (TextView) findViewById(R.id.signUp);
+        email = (EditText)findViewById(R.id.inputEmail);
+        pass = (EditText)findViewById(R.id.inputPassword);
+        firebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
         callbackManager = CallbackManager.Factory.create();
         //tf = Typeface.createFromAsset(getAssets(),"RECOGNITION.ttf");
         //textView.setTypeface(tf);
+
+        firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d("signin", "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d("signout", "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
 
         signin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,11 +116,9 @@ public class MainActivity extends AppCompatActivity {
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
 //                intent.putExtra("loginStatus", "Your id \n"+ loginResult.getAccessToken().getUserId() +
 //                        "\n"+"Your Token access \n"+loginResult.getAccessToken().getToken());
-                startActivity(intent);
-                ActivityCompat.finishAffinity(MainActivity.this);
+                handleFacebookAccessToken(loginResult.getAccessToken());
             }
 
             @Override
@@ -97,6 +131,40 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "No Network Internet!!", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void handleFacebookAccessToken(AccessToken token){
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "Auth Success!", Toast.LENGTH_SHORT).show();
+                            firebaseAuth.addAuthStateListener(firebaseAuthListener);
+                            ActivityCompat.finishAffinity(MainActivity.this);
+                            Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                            startActivity(intent);
+                        }else{
+                            Toast.makeText(MainActivity.this, "Auth Failed!", Toast.LENGTH_SHORT).show();
+                            firebaseAuth.removeAuthStateListener(firebaseAuthListener);
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(firebaseAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (firebaseAuthListener != null) {
+            firebaseAuth.removeAuthStateListener(firebaseAuthListener);
+        }
     }
 
     @Override
